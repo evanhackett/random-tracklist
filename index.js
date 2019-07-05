d95b799e6b459b0b606f0eb049f60e1d9a88961a
evanhackett <evanwhackett@gmail.com>
2019-07-04 21:52:16 -0700

    server serves html that contains a randomized list of all songs.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const get_track_list = require('./get-tracklist')
+const express = require('express')
+const shuffle = require('lodash.shuffle')
+const app = express()
+const port = 3000
 
-get_track_list((err, songs) => {
-  songs.forEach(song => {
-    console.log(song.song)
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  get_track_list((err, songs) => {
+    if (err) {
+      console.log(err)
+      return res.send(400)
+    }
+    res.render('index', {songs: shuffle(songs)})
   })
+
 })
+
+app.listen(port, () => console.log(`listening on port ${port}`))
+
--- test/test.js
+++ test/test.js
@@ -1,9 +1,28 @@
 const test = require('tape')
 const get_track_list = require('../get-tracklist')
+const request = require('request')
 
 test('get_track_list should return song data from google sheets', t => {
   get_track_list((err, songs) => {
     t.equal(songs[0].song, 'In The Street')
+    t.end()
+  })
+})
+
+const url = 'http://127.0.0.1:3000'
+
+test('server should respond to GET / with 200', t => {
+  t.comment('Some tests require the server to be running.')
+  request(url, (err, res, body) => {
+    t.equal(res.statusCode, 200)
+    t.end()
+  })
+})
+
+test('server should serve html with song list', t => {
+  request(url, (err, res, body) => {
+    t.equal(body.includes('<h1>Songs in random'), true)
+    t.equal(body.includes('<li>David Watts'), true)
     t.end()
   })
 })
