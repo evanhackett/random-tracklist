ed53c85b68f9f830a3529996a08c420731c1772d
Evan Hackett <evanwhackett@gmail.com>
2022-01-23 13:04:49 -0800

    use __dirname

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.set('views', './views')
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
